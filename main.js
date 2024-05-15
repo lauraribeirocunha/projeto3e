a10b5012b1d6e63d8be7cdda44eff9cc85c0c5d8
lauraribeirocunha <126159405+lauraribeirocunha@users.noreply.github.com>
2024-05-15 09:08:15 -0300

    blavla

--- main.js
+++ main.js
@@ -11,5 +11,29 @@ for(let i = 0; i <botoes.length; i++){
         textos[i].classList.add("ativo");
     }
 }
+
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-12-28");
+
+const tempos = [tempoObjetivo1]
+
+for (let i=0; i < contadores.length; i++){
+    contadores[i].textContent = calculaTempo(tempos[i]);
+}
+
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
+function calculaTempo(tempoObjetivo){
+    let tempoAtual = new Date();
+    let tempoFinal =- tempoObjetivo1 - tempoAtual
+    let segundos = Math.floor(tempoFinal/1000);
+    let minutos = Math.floor(segundos/60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor (horas / 24);
+
+    segundos %=60;
+    minutos %=60;
+    horas %=24;
+
+    return dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
+}
